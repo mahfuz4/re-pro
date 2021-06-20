a5f15b9726affbf80f69cf2387f0033c94b4637b
“mahfuz4” <mymsicret18@gmail.com>
2021-06-20 11:07:57 +0600

    blank

---
+++ windi.config.js
@@ -0,0 +1,19 @@
+import formsPlugin from 'windicss/plugin/forms'
+module.exports = {
+  extract: {
+    include: ['./**/*.html'],
+  },
+  darkMode:'class',
+  theme: {
+    extend: {
+      
+      container: {
+        center: true
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [formsPlugin]
+}
